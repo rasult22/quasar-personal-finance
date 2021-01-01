cf2c74229f247c578d0d47e7de8c03d3a83aed27
rasult22 <warrior.006@inbox.ru>
2021-01-01 18:54:16 +0600

    Initial setting: Set up workspace

---
+++ awesome-todo/src/pages/PageSettings.vue
@@ -0,0 +1,10 @@
+<template >
+  <q-page>
+    <p>PageSettings</p>
+  </q-page>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+</style>
---
+++ awesome-todo/src/pages/PageTodo.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page >
+    <p>PageTodo</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style lang="scss">
+
+</style>
