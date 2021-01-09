c21922e5f56fd040a18c73ab2f73f653e2aa9efd
rasult22 <warrior.006@inbox.ru>
2021-01-10 02:06:03 +0600

    Changed table order

--- awesome-todo/src/pages/MainPage/MainPage.vue
+++ awesome-todo/src/pages/MainPage/MainPage.vue
@@ -21,22 +21,22 @@
       <q-markup-table flat bordered>
         <thead class="bg-blue-1">
           <tr >
+            <th class="text-left">Cash</th>
+            <th class="text-left">Operation type</th>
             <th class="text-left ">Comments</th>
             <th class="text-left">Category</th>
-            <th class="text-left">Cash</th>
             <th class="text-left">Date</th>
-            <th class="text-left">Operation type</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="operation in operations" 
           :key="operation.date + operation.amount + operation.comments" 
           >
+            <td class="text-left">{{operation.postType === 'income' ? '+' : '-'}} {{`${operation.amount} ${operation.currency}`}}</td>
+            <td class="text-left">{{ operation.postType}}</td>
             <td :class="{'tr-expense': operation.postType === 'expense', 'tr-income' :operation.postType === 'income'}" class="text-left">{{operation.comments}}</td>
             <td class="text-left">{{operation.category}}</td>
-            <td class="text-left">{{operation.postType === 'income' ? '+' : '-'}} {{`${operation.amount} ${operation.currency}`}}</td>
             <td class="text-left">{{ operation.date }}</td>
-            <td class="text-left">{{ operation.postType}}</td>
           </tr>
         </tbody>
       </q-markup-table>
