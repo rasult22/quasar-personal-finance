093afe9d59c67acb928e626626530faf16e6f69e
rasult22 <warrior.006@inbox.ru>
2021-05-23 17:02:05 +0600

    5 minute work

--- backend/notes/middleware.js
+++ backend/notes/middleware.js
@@ -1,13 +1,17 @@
 const express = require('express')
-
+const morgan = require('morgan')
 const app = express()
+
+app.use(morgan('dev'))
+
+app.use(express.json())
 
 app.use(middleware)
 
 
 function middleware (req, res, next) {
   req.requestTime = new Date().toISOString()
-  console.log('Hello from the middleware', req.requestTime)
+  next()
 }
 
 app.listen(5000, () => console.log('started....'))
