303c6a4dec4e29173ed3c4b1ad7b5abd6c4d5f07
Itachi <warrior.006@inbox.ru>
2021-06-16 22:22:38 +0600

    feature: delete and update endpoints for users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,11 +19,39 @@ exports.createUser = async (req, res) => {
     })
   }
 }
-exports.updateUser = (req, res) => {
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
 
+    res.status(200).json({
+      status: 'success',
+      data: { user }
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    })
+  }
 }
-exports.deleteUser = (req, res) => {
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
 
+    res.status(204).json({
+      status: 'success',
+      data: null
+    })
+
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    })
+  }
 }
 
 exports.getUsers = async (req, res) => {
