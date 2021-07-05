1eb07f6c5c7cf0cf267d5211ddece2f543e0c885
rasult22 <warrior.006@inbox.ru>
2021-07-05 23:12:28 +0600

    Distinguishing handling different type of errors

--- backend/controllers/error.js
+++ backend/controllers/error.js
@@ -1,8 +1,40 @@
+const sendErrorDev = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+    error
+  })
+}
+const sendErrorProd = (error, res) => {
+  // Operational, trusted error: send message to client
+  if(error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message
+    })
+
+    // Programming or other unknown error: don't leak error details 
+  } else {
+    // 1. Log error
+    console.error('ERROR', error)
+
+    // 2. Send generic error
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong'
+    })
+  }
+}
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error'
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message
-  })
+ 
+  if(process.env.NODE_ENV !== 'production') {
+    sendErrorDev(error, res)
+  } else {
+    sendErrorProd(error, res)
+  }
+ 
 }
