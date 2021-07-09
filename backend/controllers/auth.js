fee261c8c5662138677b5b73f3b5d5653dc2dcd8
Itachi <warrior.006@inbox.ru>
2021-07-09 11:08:10 +0600

    update password

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require('../models/userModel')
-const { promisify } = require('util')
+const crypto = require('crypto')
 const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
 const jwt =  require('jsonwebtoken')
@@ -126,6 +126,39 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 })
 
-exports.resetPassword = (req, res, next) => {
+exports.resetPassword = async (req, res, next) => {
+  
+  // 1) Get user basen on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+   
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  })
 
+  // 2) If token has not expired, and there is user, set the new password
+  if(!user) return next(new AppError('Token is invalid or has expired', 400))
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+
+  await user.save()
+
+  // 3) Update changedPasswordAt property for the user
+  // Implemented via mongoose model pre save middleware 
+
+
+  // 4) Log the user in, send JWT
+
+  const token = signToken(user._id)
+
+  res.status(200).json({
+    status: 'success',
+    token
+  })
 }
