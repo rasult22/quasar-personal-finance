dee72a83febeece78c8d62c75cff30805a40e6ea
Itachi <warrior.006@inbox.ru>
2021-07-12 16:03:17 +0600

    Data sanitization

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 const morgan = require('morgan')
 const path = require('path')
 const AppError = require('./utils/appError')
@@ -23,6 +25,12 @@ if(process.env.NODE_ENV === 'development') {
 app.use(express.json(), {
   limit: '10kb'
 })
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
 
 // serving static files
 app.use(express.static(`${__dirname}/public`))
