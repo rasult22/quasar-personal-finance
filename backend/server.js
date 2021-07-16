ca567eb31d20113eb859707b86d3a9d0c03591f0
Itachi <warrior.006@inbox.ru>
2021-07-16 18:06:31 +0600

    data modeling notes  and parameter pollution fix

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
+const hpp = require('hpp')
 const morgan = require('morgan')
 const path = require('path')
 const AppError = require('./utils/appError')
@@ -22,15 +23,22 @@ if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 // Body parser, reading data from body into req.body
-app.use(express.json(), {
+app.use(express.json( {
   limit: '10kb'
-})
+}),)
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize())
 
 // Data sanitization against XSS
 app.use(xss())
+
+// Parameter pollution fix
+app.use(hpp({
+  whitelist:[
+    // here I can whitelist some parameters
+  ]
+}))
 
 // serving static files
 app.use(express.static(`${__dirname}/public`))
