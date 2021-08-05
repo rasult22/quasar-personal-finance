f90450db721a7180bb206924c159fdd84db62808
Itachi-MacOS <warrior.006@inbox.ru>
2021-08-06 02:41:05 +0600

    Компонент карточки

---
+++ awesome-todo/src/components/AppCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-card">
+      <div class="app-card__title">
+        <slot name="title">
+        </slot>
+      </div>
+      <div class="app-card__content">
+        <slot>
+        </slot>
+      </div>
+      <slot name="controls" class="app-card__controls">
+
+      </slot>
+  </div>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .app-card {
+    display:flex;
+    flex-wrap: wrap;
+    padding: 10px;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+    &__title {
+      width: 100%;
+      font-weight: bold;
+      font-size: 1.2rem;
+    }
+  }
+</style>
