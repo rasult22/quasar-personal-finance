e4320a54967c78eb9a543830e203fdce582238b4
Itachi-MacOS <warrior.006@inbox.ru>
2021-08-06 02:41:23 +0600

    Добавление имени пользователя в шапку

--- awesome-todo/src/components/Header.vue
+++ awesome-todo/src/components/Header.vue
@@ -2,6 +2,7 @@
   <q-header class="bg-white app-header" elevated>
     <q-toolbar>
       <q-toolbar-title class="app-header__title"> {{ title }}</q-toolbar-title>
+      <span class="app-header__right-title"> {{ rightTitle }}</span>
     </q-toolbar>
   </q-header>
 </template>
@@ -12,6 +13,10 @@ export default {
     title: {
       type: String,
       default: 'Header'
+    },
+    rightTitle: {
+      type: String,
+      default: ''
     }
   },
   data() {
@@ -31,6 +36,10 @@ $app-header-title-color: #633fe8;
 
   &__title {
     color: $app-header-title-color;
+  }
+  &__right-title {
+    color: black;
+    font-weight: 400;
   }
 }
 
